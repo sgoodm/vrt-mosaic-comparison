f9b39207cca114f4c8703e3bbcc94f2f7afcf144
Seth Goodman <sgoodman@aiddata.wm.edu>
2021-10-12 12:21:59 -0400

    Add framework for script to test generating and reading of data

---
+++ gen_data.py
@@ -0,0 +1,36 @@
+'''
+Generate new data for testing
+'''
+
+from pathlib import Path
+import numpy as np
+import rasterio
+
+base_path = Path('data')
+
+
+tiles_path = base_path / 'tiles'
+tiles_path.mkdir(exist_ok=True)
+
+# define individual tiles/scenes bounds/size/transform
+#
+
+# generate data for each tile
+#
+
+# save each tile as geotiff
+#
+
+
+# -------------------------------------
+
+
+# generate vrt
+#
+
+
+# -------------------------------------
+
+
+# generate mosaiced geotiff
+#
---
+++ read_data.py
@@ -0,0 +1,20 @@
+'''
+Read data from VRT and mosaiced GeoTIFF
+'''
+
+
+from pathlib import Path
+import rasterio
+
+
+# define series of windows/points/etc to read
+#
+
+
+# read data from VRT
+#
+
+
+# read data from mosaiced GeoTIFF
+#
+
