727d3e34bbf0d61f1cbb6f69c74667157b172cb8
Seth Goodman <sgoodman@aiddata.wm.edu>
2021-10-12 14:14:32 -0400

    Add framing for reading comparison

--- read_data.py
+++ read_data.py
@@ -4,7 +4,21 @@ Read data from VRT and mosaiced GeoTIFF
 
 
 from pathlib import Path
+import atexit
 import rasterio
+from rasterio.merge import merge
+from rasterio.vrt import WarpedVRT
+from rasterio.windows import from_bounds
+import line_profiler
+
+# Start a profiler
+profile = line_profiler.LineProfiler()
+atexit.register(profile.print_stats)
+
+
+base_path = Path('data')
+vrt_path = base_path / 'test_vrt.vrt'
+mosaic_path = base_path / 'test_mosaic.tif'
 
 
 # define series of windows/points/etc to read
@@ -12,9 +26,21 @@ import rasterio
 
 
 # read data from VRT
-#
+
+@profile
+def fromVRT(bounds_or_window):
+    with rasterio.Env():
+        with rasterio.open(vrt_path) as src:
+            with WarpedVRT(src) as vrt:
+                r = vrt.read(1, window=win)
+
 
 
 # read data from mosaiced GeoTIFF
-#
+
+@profile
+def fromMosaic(bounds_or_window):
+    with rasterio.Env():
+        with rasterio.open(mosaic_path) as src:
+            r = src.read(1, window=win)
 
