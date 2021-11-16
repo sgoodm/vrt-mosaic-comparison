21a1f2ca16ee05308cc5e73b1866c81b9871e659
Jacob Hall <email@jacobhall.net>
2021-11-16 12:39:56 -0500

    functionizing
    - broke gen_data.py out into functions that can be called
      individually, and added generate_all() to call them all at
      once

--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,6 @@
-'''
+"""
 Generate new data for testing
-'''
+"""
 
 from pathlib import Path
 import numpy as np
@@ -10,84 +10,97 @@ from osgeo import gdal
 from affine import Affine
 
 
-base_path = Path('data')
+def gen_bounds_tiles(tiles_path, resolution, tile_sqrt) -> list:
+    bounds_list = []
+    for i in range(tile_sqrt):
+        xmin = i * (360 / tile_sqrt) - 180
+        xmax = (i + 1) * (360 / tile_sqrt) - 180
+        for j in range(tile_sqrt):
+            ymin = j * (180 / tile_sqrt) - 90
+            ymax = (j + 1) * (180 / tile_sqrt) - 90
+            bounds_list.append((xmin, ymin, xmax, ymax))
+    # xmin, ymin, xmax, ymax
+
+    transform_list = [
+        Affine(resolution, 0, i[0], 0, -resolution, i[3]) for i in bounds_list
+    ]
+    dims_list = [
+        rasterio.transform.rowcol(
+            transform_list[i], bounds_list[i][2], bounds_list[i][1]
+        )
+        for i in range(len(transform_list))
+    ]
+
+    tile_list = []
+    for i, (transform, dims) in enumerate(zip(transform_list, dims_list)):
+        # create empty raster
+        out_img = np.random.random(size=dims)
+        out_meta = {
+            "driver": "GTiff",
+            "height": dims[0],
+            "width": dims[1],
+            "transform": transform,
+            "crs": "epsg:4326",
+            "dtype": "float64",
+            "count": 1,
+            "nodata": 0,
+        }
+        out_path = tiles_path / f"tile_{i}.tif"
+        tile_list.append(str(out_path))
+        with rasterio.open(out_path, "w", **out_meta) as dst:
+            dst.write(out_img, 1)
+    return bounds_list, tile_list
 
 
-tiles_path = base_path / 'tiles'
-tiles_path.mkdir(parents=True, exist_ok=True)
-
-# should we "warp" the VRT?
-warp = False
-
-# define individual tiles/scenes bounds/size/transform
-resolution = 0.5
-
-# define number of tiles (square this number)
-tile_sqrt = 2
-
-bounds_list = []
-for i in range(tile_sqrt):
-    xmin = i * (360 / tile_sqrt) - 180
-    xmax = (i + 1) * (360 / tile_sqrt) - 180
-    for j in range(tile_sqrt):
-        ymin = j * (180 / tile_sqrt) - 90
-        ymax = (j + 1) * (180 / tile_sqrt) - 90
-        bounds_list.append((xmin, ymin, xmax, ymax))
-# xmin, ymin, xmax, ymax
-
-transform_list = [ Affine(resolution, 0, i[0], 0, -resolution, i[3]) for i in bounds_list ]
-
-dims_list = [rasterio.transform.rowcol(transform_list[i], bounds_list[i][2], bounds_list[i][1]) for i in range(len(transform_list)) ]
-
-# generate data for each tile
-# save each tile as geotiff
-
-tile_list = []
-for i, (transform, dims) in enumerate(zip(transform_list, dims_list)):
-    # create empty raster
-    out_img = np.random.random(size=dims)
-    out_meta = {
-        'driver': 'GTiff',
-        'height': dims[0],
-        'width': dims[1],
-        'transform': transform,
-        'crs': 'epsg:4326',
-        'dtype': 'float64',
-        'count': 1,
-        'nodata': 0
-    }
-    out_path = tiles_path / f'tile_{i}.tif'
-    tile_list.append(str(out_path))
-    with rasterio.open(out_path, 'w', **out_meta) as dst:
-        dst.write(out_img, 1)
-
-# -------------------------------------
+def gen_vrt(vrt_path, tile_list, warp=False):
+    """
+    generate vrt
+    vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic', addAlpha=False)
+    """
+    my_vrt = gdal.BuildVRT(str(vrt_path), tile_list)  # , options=vrt_options)
+    if warp == True:
+        gdal.Warp(str(vrt_path), my_vrt)
+    del my_vrt
 
 
-# generate vrt
-# vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic', addAlpha=False)
-vrt_path = base_path / 'test_vrt.vrt'
-my_vrt = gdal.BuildVRT(str(vrt_path), tile_list)#, options=vrt_options)
-if warp == True:
-    gdal.Warp(str(vrt_path), my_vrt)
-del my_vrt
+def gen_mosaic(bounds_list, tile_list):
+    """
+    generate mosaiced geotiff
+    """
+    bounds = (
+        min([i[0] for i in bounds_list]),
+        min([i[1] for i in bounds_list]),
+        max([i[2] for i in bounds_list]),
+        max([i[3] for i in bounds_list]),
+    )
+    sources = [rasterio.open(raster) for raster in tile_list]
+    mosaic, out_transform = merge(sources, bounds=bounds)
+    out_meta = sources[0].meta.copy()
+    out_meta.update(
+        {
+            "height": mosaic.shape[1],
+            "width": mosaic.shape[2],
+            "transform": out_transform,
+        }
+    )
+    mosaic_path = base_path / "test_mosaic.tif"
+    with rasterio.open(str(mosaic_path), "w", **out_meta) as dst:
+        dst.write(mosaic)
 
 
-# -------------------------------------
+def generate_all(base_path, resolution, tile_sqrt):
+    # Create directory to hold tiles
+    tiles_path = base_path / "tiles"
+    tiles_path.mkdir(parents=True, exist_ok=True)
+    # Generate tile bounds, tiles
+    bounds_list, tile_list = gen_bounds_tiles(tiles_path, resolution, tile_sqrt)
+    # Generate VRT
+    vrt_path = base_path / "test_vrt.vrt"
+    gen_vrt(vrt_path, tile_list)
+    # Generate mosaic
+    gen_mosaic(bounds_list, tile_list)
 
 
-# generate mosaiced geotiff
-bounds =(min([i[0] for i in bounds_list]), min([i[1] for i in bounds_list]), max([i[2] for i in bounds_list]), max([i[3] for i in bounds_list]))
-sources = [rasterio.open(raster) for raster in tile_list]
-mosaic, out_transform = merge(sources, bounds=bounds)
-out_meta = sources[0].meta.copy()
-out_meta.update(
-    {
-        "height": mosaic.shape[1],
-        "width": mosaic.shape[2],
-        "transform": out_transform
-    }
-)
-mosaic_path = base_path / 'test_mosaic.tif'
-with rasterio.open(str(mosaic_path), "w", **out_meta) as dst:
-    dst.write(mosaic)
+if __name__ == "__main__":
+    base_path = Path("data")
+    generate_all(base_path, resolution=0.5, tile_sqrt=4)
